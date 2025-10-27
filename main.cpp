b4b10d1b638db20c0a5b8ffe2767fecdd9f3b5ed
admin <admin@localhost.localdomain>
2025-10-27 16:30:02 +0700

    исправлено приветствие

--- main.cpp
+++ main.cpp
@@ -7,6 +7,6 @@ int main(int argc, char** argv) {
     char* str = (char*)malloc(sizeof(char) * SIZE);
     printf("Введите своё имя: \n");
     scanf("%s", str);
-    printf("Привет, %s", str);
-    return 0;
+    puts("Привет");
+    puts(str);
 }
